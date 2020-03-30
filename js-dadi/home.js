799ce639ef1428d5559b3a41a791ef960c2b5a62
Nicolò Battaglia <gaara0633@gmail.com>
2020-03-30 10:31:59 +0200

    fix

--- js-dadi/home.js
+++ js-dadi/home.js
@@ -1,6 +1,10 @@
 //Gioco dei dadi, chi fa di più vince.
 var dadiUtente = Math.floor(Math.random() * 6) + 1;
+alert("Hai lanciato il dado, il tuo numero è: " + dadiUtente)
+console.log(dadiUtente);
 var dadiCPU = Math.floor(Math.random() * 6) + 1;
+alert("Il pc ha lanciato il dado, il suo numero è: " + dadiCPU)
+console.log(dadiCPU);
 var vincitore;
 if (dadiUtente > dadiCPU) {
   vincitore = "Utente ha vinto!"
@@ -9,6 +13,4 @@ if (dadiUtente > dadiCPU) {
 } else{
   vincitore = "Pareggio!"
 }
-console.log(dadiUtente);
-console.log(dadiCPU);
 document.getElementById('mio_id').innerHTML = vincitore;
--- js-mail/home.js
+++ js-mail/home.js
@@ -1,23 +1,17 @@
 //Chiedi all’utente la sua email controlla che sia nella lista di chi può accedere e stampa un messaggio appropriato
 var emailIscritte = ["matteo@gmail.com", "simone@gmail.com", "nicolo@gmail.com", "stefano@gmail.com", "luca@gmail.com", "daniel@gmail.com", "giuseppe@gmail.com"];
-var emailUtente = prompt("Inserisci la tua mail per verificare se sei registrato nel nostro database")
-var benvenuto;
-if (emailIscritte[0] === emailUtente) {
-  benvenuto = "Sei presente nel nostro database, benvenuto :D"
-}else if (emailIscritte[1] === emailUtente){
-  benvenuto = "Sei presente nel nostro database, benvenuto :D"
-}else if (emailIscritte[2] === emailUtente){
-  benvenuto = "Sei presente nel nostro database, benvenuto :D"
-}else if (emailIscritte[3] === emailUtente){
-  benvenuto = "Sei presente nel nostro database, benvenuto :D"
-}else if (emailIscritte[4] === emailUtente){
-  benvenuto = "Sei presente nel nostro database, benvenuto :D"
-}else if (emailIscritte[5] === emailUtente){
-  benvenuto = "Sei presente nel nostro database, benvenuto :D"
-}else if (emailIscritte[6] === emailUtente){
-  benvenuto = "Sei presente nel nostro database, benvenuto :D"
-}else{
-  benvenuto = "Non sei presente nel nostro database, riprova o esci prima che sia troppo tardi"
-}
 console.log(emailIscritte);
+var emailUtente = prompt("Inserisci la tua mail per verificare se sei registrato nel nostro database");
+var check = false;
+var benvenuto;
+for (var i = 0; i < emailIscritte.length; i++) {
+  if (emailUtente === emailIscritte[i]){
+    check = true;
+  }
+}
+if (check) {
+  benvenuto = "Sei benvenuto all' interno della nosta lista :D";
+}else{
+  benvenuto = "Riprova oppure esci prima che sia troppo tardi D:";
+}
 document.getElementById('mio_id').innerHTML = benvenuto;
